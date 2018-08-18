355258cbd1bc8d7f400e6c61451dfe07414e70db
yangjz1125 <yangjz1125@outlook.com>
2018-08-18 08:38:31 +0800

    Debug for datagen.

--- src/datagen.c
+++ src/datagen.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <time.h>
 
 //#include <ldev/lib/lstring.h>
 
@@ -15,19 +16,19 @@ int ints[MAX_VAR_COUNT];
 #define NEXT_WORD scanf("%s", tmp)
 
 void gen_keys(char *key){
-	if(!strcmp(key, "ENDL"))
+	if(!strcmp(key, "ENDL") || !strcmp(key, "E"))
 		printf("\n");
-	else if(!strcmp(key, "SPACE"))
+	else if(!strcmp(key, "SPACE") || !strcmp(key,"S"))
 		printf(" ");
-	else if(!strcmp(key, "BREAK"))
+	else if(!strcmp(key, "BREAK") || !strcmp(key, "B"))
 		exit(0);
-	else if(!strcmp(key, "RANDOM")){
+	else if(!strcmp(key, "RANDOM") || !strcmp(key, "R")){
 		printf("%d",rand());
-	} else if(!(strcmp(key, "RANDOM_STRU") || strcmp(key, "RANDOM_STR"))){
+	} else if(!(strcmp(key, "RANDOM_STRU") && strcmp(key, "RANDOM_STR") && strcmp(key, "RSTR") && strcmp(key, "RSTRU"))){
 		int cnt;
 		scanf("%d", &cnt);
 		for(;cnt > 0; --cnt)printf("%c", rand() % 26 + 'A');
-	} else if(!strcmp(key, "RANDOM_STRL")){
+	} else if(!strcmp(key, "RANDOM_STRL") || !strcmp(key, "RSTRL")){
 		int cnt;
 		scanf("%d", &cnt);
 		for(;cnt > 0; --cnt)printf("%c", rand() % 26 + 'a');
@@ -40,6 +41,9 @@ void gen_keys(char *key){
 
 void scan(void){
 	NEXT_WORD;
+	if(!strcmp(tmp, ";")){
+		exit(0);
+	}
         if(isupper(*tmp)){
 		gen_keys(tmp);
 	}
@@ -47,6 +51,7 @@ void scan(void){
 
 int main(){
 	tmp = malloc(MAX_WORD_SIZE);
+	srand(time(NULL) + clock());
 	while(!feof(stdin)){
 		scan();
 	}

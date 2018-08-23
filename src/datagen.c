ed245935a96477efc2a8c69d6f957cb5d3c7355a
yangjz <yangjz1125@outlook.com>
2018-08-23 10:01:16 +0800

    update for datagen

--- src/datagen.c
+++ src/datagen.c
@@ -11,7 +11,10 @@
 char *tmp;
 
 int ints[MAX_VAR_COUNT];
-//lstring *strings[MAX_VAR_COUNT]；
+char *strings[MAX_VAR_COUNT];
+double floats[MAX_VAR_COUNT];
+
+void scan(void);
 
 #define NEXT_WORD scanf("%s", tmp)
 
@@ -24,7 +27,25 @@ void gen_keys(char *key){
 		exit(0);
 	else if(!strcmp(key, "RANDOM") || !strcmp(key, "R")){
 		printf("%d",rand());
-	} else if(!(strcmp(key, "RANDOM_STRU") && strcmp(key, "RANDOM_STR") && strcmp(key, "RSTR") && strcmp(key, "RSTRU"))){
+	} else if(!strcmp(key, "LET")){
+		char *name = tmp;
+		scanf("%s", tmp);
+		        if(*name == 'i'){
+				unsigned int index;
+				sscanf(name+1, "%u", &index);
+				scanf("%d", ints+index);
+			} else if(*name == 's'){
+				unsigned int index;
+				sscanf(name+1, "%u", &index);
+				free(strings[index]);
+				strings[index] = malloc(128);
+				scanf("%s", strings[index]);
+			} else if(*name == 'f'){
+				unsigned int index;
+				sscanf(name+1, "%u", &index);
+				scanf("%lf", floats+index);
+			}		
+	}else if(!(strcmp(key, "RANDOM_STRU") && strcmp(key, "RANDOM_STR") && strcmp(key, "RSTR") && strcmp(key, "RSTRU"))){
 		int cnt;
 		scanf("%d", &cnt);
 		for(;cnt > 0; --cnt)printf("%c", rand() % 26 + 'A');
@@ -39,6 +60,22 @@ void gen_keys(char *key){
 	}
 }
 
+void gen_vars(char *name){
+        if(*name == 'i'){
+	        unsigned int index;
+		sscanf(name+1, "%u", &index);
+		printf("%d", ints[index]);
+	} else if(*name == 's'){
+		unsigned int index;
+		sscanf(name+1, "%u", &index);
+		printf("%s", strings[index]);
+	} else if(*name == 'f'){
+		unsigned int index;
+		sscanf(name+1, "%u", &index);
+		printf("%lf", floats[index]);
+	}
+}
+
 void scan(void){
 	NEXT_WORD;
 	if(!strcmp(tmp, ";")){
@@ -46,11 +83,16 @@ void scan(void){
 	}
         if(isupper(*tmp)){
 		gen_keys(tmp);
+	} else {
+		gen_vars(tmp);
 	}
 }
 
 int main(){
 	tmp = malloc(MAX_WORD_SIZE);
+	memset(ints, 0 ,sizeof(ints));
+	memset(strings, 0 , sizeof(strings));
+	memset(floats, 0 , sizeof(floats));
 	srand(time(NULL) + clock());
 	while(!feof(stdin)){
 		scan();
